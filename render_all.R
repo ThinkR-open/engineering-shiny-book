b8f0393e3f2e9f54dd3a1cb71c0ce72e07a17f10
colin <colin@thinkr.fr>
2019-08-09 22:49:37 +0200

    update header numerotation for index.Rmd

---
+++ render_all.R
@@ -0,0 +1,3 @@
+bookdown::render_book("index.Rmd")
+bookdown::render_book("index.Rmd", output_format = "bookdown::epub_book")
+bookdown::render_book("index.Rmd", output_format = "bookdown::pdf_book")
