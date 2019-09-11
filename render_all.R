9afaf38e725ac012a75a77cad262367e5420c5ff
colin <colin@thinkr.fr>
2019-09-11 21:09:31 +0200

    changed format

--- render_all.R
+++ render_all.R
@@ -1,3 +1,1 @@
 bookdown::render_book("index.Rmd")
-bookdown::render_book("index.Rmd", output_format = "bookdown::epub_book")
-bookdown::render_book("index.Rmd", output_format = "bookdown::pdf_book")
