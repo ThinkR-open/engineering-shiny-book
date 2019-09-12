370e3c3a031c377a71b337fb68f31b9e34325424
colin <colin@thinkr.fr>
2019-09-12 21:16:20 +0200

    update the render script

--- render_all.R
+++ render_all.R
@@ -1,1 +1,12 @@
+wip <- TRUE
+if (wip){
+  x <- yaml::read_yaml("_bookdown.yml")
+  x$output_dir <- "docs/wip"
+  yaml::write_yaml(x, "_bookdown.yml")
+}
 bookdown::render_book("index.Rmd")
+git2r::add(".", ".")
+git2r::commit(
+  ".", message = sprintf("%s book build", Sys.time())
+    )
+system("git push")
