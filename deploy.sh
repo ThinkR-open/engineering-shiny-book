aba010a8a49e7a72da7be5f8d2bdbe3cba7eb5d2
statnmap <statnmap@users.noreply.github.com>
2019-10-14 23:44:51 +0200

    Set travis like Geocomputation with R
    source: https://github.com/Robinlovelace/geocompr

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -o errexit -o nounset
+BASE_REPO=$PWD
+
+update_website() {
+  cd ..; mkdir gh-pages; cd gh-pages
+  git init
+  git config user.name "Sébastien Rochette"
+  git config user.email "sebastienrochettefr@gmail.com"
+  git config --global push.default simple
+  git remote add upstream "https://$GH_TOKEN@github.com/ThinkR-open/building-shiny-apps-workflow.git"
+  git fetch upstream 2>err.txt
+  git checkout gh-pages
+  
+  cp -fvr $BASE_REPO/_book/* .
+  git add *.html libs/ figures images/ *.json
+  git commit -a -m "Updating book (${TRAVIS_BUILD_NUMBER})"
+  git status
+  git push 2>err.txt
+  cd ..
+}
+
+update_website
