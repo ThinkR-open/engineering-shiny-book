790f85b4549ca6870deaff1ca41feface166c71a
Sébastien <statnmap@users.noreply.github.com>
2020-01-22 08:59:49 +0100

    Review big-shiny.Rmd
    *big-shiny.Rmd*
    - Reword first sentences of paragraph to target the aim of the paragraph
    - Remove notions of "simple", "easy"
    - Shorten sentences
    - Remove superlatives : "very strict"
    - Expand verbs reduction: don't => do not, it's => it is, ...
    - Repeat words instead of "these means", "reducing it", ...=> Reducing
    complexity means ...
    - Move types of complexity to higher title level
    - Move Balancing complexity to higher title level
    
    *Others*
    - Run dev_history.R to update dependencies in DESCRIPTION

--- dev_history.R
+++ dev_history.R
@@ -1,8 +1,8 @@
 usethis::use_build_ignore("dev_history.R")
 
 # bookdown Imports are in Rmds
-imports <- c("bookdown", attachment::att_from_rmds("."))
 # remotes::install_github("ThinkR-open/attachment")
+imports <- c("bookdown", attachment::att_from_rmds("."))
 attachment::att_to_desc_from_is(path.d = "DESCRIPTION", imports = imports)
 
 # Install dependencies
