0c4f4ee886fc711377b8758f3ee4275306b5f797
Sébastien <statnmap@users.noreply.github.com>
2020-02-10 22:44:09 +0100

    update makefile for travis book creation

--- dev_history.R
+++ dev_history.R
@@ -10,4 +10,14 @@ attachment::att_to_desc_from_is(path.d = "DESCRIPTION", imports = imports)
 #   attachment::att_from_description() 
 # )
 # ou bien
-remotes::install_local()
+remotes::install_local(force = TRUE)
+
+# Test
+pid <- rstudioapi::terminalExecute(
+  'R -e \'bookdown::render_book("index.Rmd", output_format = "bookdown::gitbook", clean = FALSE, output_dir = "docs/wip") \''
+)
+rstudioapi::terminalKill(pid)
+
+
+bookdown::render_book("index.Rmd", output_format = "bookdown::gitbook", clean = FALSE, output_dir = "docs/wip")
+
