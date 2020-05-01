814231e994ac7580f75538118ba6c2ce9ff460a6
colin <colin@thinkr.fr>
2020-05-01 10:32:06 +0200

    series of updates after re-reading

--- before-build.R
+++ before-build.R
@@ -41,3 +41,34 @@ lapply(
     namer::name_chunks(x)
   }
 )
+
+gh::gh(
+  "POST /repos/:owner/:repo/issues",
+  owner = gsub("([^/]*)/.*", "\\1", Sys.getenv("GITHUB_REPOSITORY")),
+  repo = gsub("[^/]*/(.*)", "\\1", Sys.getenv("GITHUB_REPOSITORY")),
+  title = sprintf(
+    "Spellcheck GA %s - %s", 
+    Sys.getenv("GITHUB_ACTION"), Sys.Date()
+  ), 
+  .token = Sys.getenv("GH_TOKEN"),
+  body = paste(
+    capture.output(
+      knitr::kable(
+        do.call(
+          rbind, 
+          lapply(
+            list.files(
+              path = ".", 
+              pattern = ".Rmd$"
+            ), 
+            function(x){
+              spelling::spell_check_files(x)
+            }
+          )
+        )
+      )
+    ),
+    collapse = "\n"
+  )
+)
+
