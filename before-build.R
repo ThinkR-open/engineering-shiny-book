2cce7f82e08e6113ca29316ed7ea6351a56e3e14
colin <colin@thinkr.fr>
2020-06-10 12:47:47 +0200

    install from github

--- before-build.R
+++ before-build.R
@@ -1,5 +1,5 @@
 install.packages(c("attachment", "remotes", "usethis", "namer", "desc", "spelling"))
-remotes::install_version("subprocess", "0.8.3")
+remotes::install_github("lbartnik/subprocess")
 remotes::install_github("rstudio/websocket", "rc-v1.2.0")
 paks <- c(
   "cloc", "dplyr", "cyclocomp", "tidytuesday201942", "shiny", "packageMetrics2", 
