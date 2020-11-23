e9e01c9de9a683c5d649e0fa8c5b78d01bd01b80
colin <colin@thinkr.fr>
2020-11-23 08:58:01 +0100

    blankgolem

---
+++ blankgolem/dev/01_start.R
@@ -0,0 +1,65 @@
+# Building a Prod-Ready, Robust Shiny Application.
+# 
+# README: each step of the dev files is optional, and you don't have to 
+# fill every dev scripts before getting started. 
+# 01_start.R should be filled at start. 
+# 02_dev.R should be used to keep track of your development during the project.
+# 03_deploy.R should be used once you need to deploy your app.
+# 
+# 
+########################################
+#### CURRENT FILE: ON START SCRIPT #####
+########################################
+
+## Fill the DESCRIPTION ----
+## Add meta data about your application
+## 
+## /!\ Note: if you want to change the name of your app during development, 
+## either re-run this function, call golem::set_golem_name(), or don't forget
+## to change the name in the app_sys() function in app_config.R /!\
+## 
+golem::fill_desc(
+  pkg_name = "blankgolem", # The Name of the package containing the App 
+  pkg_title = "PKG_TITLE", # The Title of the package containing the App 
+  pkg_description = "PKG_DESC.", # The Description of the package containing the App 
+  author_first_name = "AUTHOR_FIRST", # Your First Name
+  author_last_name = "AUTHOR_LAST", # Your Last Name
+  author_email = "AUTHOR@MAIL.COM", # Your Email
+  repo_url = NULL # The URL of the GitHub Repo (optional) 
+)     
+
+## Set {golem} options ----
+golem::set_golem_options()
+
+## Create Common Files ----
+## See ?usethis for more information
+usethis::use_mit_license( name = "Golem User" )  # You can set another license here
+usethis::use_readme_rmd( open = FALSE )
+usethis::use_code_of_conduct()
+usethis::use_lifecycle_badge( "Experimental" )
+usethis::use_news_md( open = FALSE )
+
+## Use git ----
+usethis::use_git()
+
+## Init Testing Infrastructure ----
+## Create a template for tests
+golem::use_recommended_tests()
+
+## Use Recommended Packages ----
+golem::use_recommended_deps()
+
+## Favicon ----
+# If you want to change the favicon (default is golem's one)
+golem::use_favicon() # path = "path/to/ico". Can be an online file. 
+golem::remove_favicon()
+
+## Add helper functions ----
+golem::use_utils_ui()
+golem::use_utils_server()
+
+# You're now set! ----
+
+# go to dev/02_dev.R
+rstudioapi::navigateToFile( "dev/02_dev.R" )
+
---
+++ blankgolem/dev/02_dev.R
@@ -0,0 +1,95 @@
+# Building a Prod-Ready, Robust Shiny Application.
+# 
+# README: each step of the dev files is optional, and you don't have to 
+# fill every dev scripts before getting started. 
+# 01_start.R should be filled at start. 
+# 02_dev.R should be used to keep track of your development during the project.
+# 03_deploy.R should be used once you need to deploy your app.
+# 
+# 
+###################################
+#### CURRENT FILE: DEV SCRIPT #####
+###################################
+
+# Engineering
+
+## Dependencies ----
+## Add one line by package you want to add as dependency
+usethis::use_package( "thinkr" )
+
+## Add modules ----
+## Create a module infrastructure in R/
+golem::add_module( name = "name_of_module1" ) # Name of the module
+golem::add_module( name = "name_of_module2" ) # Name of the module
+
+## Add helper functions ----
+## Creates ftc_* and utils_*
+golem::add_fct( "helpers" ) 
+golem::add_utils( "helpers" )
+
+## External resources
+## Creates .js and .css files at inst/app/www
+golem::add_js_file( "script" )
+golem::add_js_handler( "handlers" )
+golem::add_css_file( "custom" )
+
+## Add internal datasets ----
+## If you have data in your package
+usethis::use_data_raw( name = "my_dataset", open = FALSE ) 
+
+## Tests ----
+## Add one line by test you want to create
+usethis::use_test( "app" )
+
+# Documentation
+
+## Vignette ----
+usethis::use_vignette("blankgolem")
+devtools::build_vignettes()
+
+## Code Coverage----
+## Set the code coverage service ("codecov" or "coveralls")
+usethis::use_coverage()
+
+# Create a summary readme for the testthat subdirectory
+covrpage::covrpage()
+
+## CI ----
+## Use this part of the script if you need to set up a CI
+## service for your application
+## 
+## (You'll need GitHub there)
+usethis::use_github()
+
+# GitHub Actions
+usethis::use_github_action() 
+# Chose one of the three
+# See https://usethis.r-lib.org/reference/use_github_action.html
+usethis::use_github_action_check_release() 
+usethis::use_github_action_check_standard() 
+usethis::use_github_action_check_full() 
+# Add action for PR
+usethis::use_github_action_pr_commands()
+
+# Travis CI
+usethis::use_travis() 
+usethis::use_travis_badge() 
+
+# AppVeyor 
+usethis::use_appveyor() 
+usethis::use_appveyor_badge()
+
+# Circle CI
+usethis::use_circleci()
+usethis::use_circleci_badge()
+
+# Jenkins
+usethis::use_jenkins()
+
+# GitLab CI
+usethis::use_gitlab_ci()
+
+# You're now set! ----
+# go to dev/03_deploy.R
+rstudioapi::navigateToFile("dev/03_deploy.R")
+
