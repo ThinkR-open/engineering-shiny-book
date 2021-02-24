100291a01a89a0f16348208f95637ee85d589fa4
colin <colin@thinkr.fr>
2021-02-24 16:15:44 +0100

    new redirects

--- redirect.R
+++ redirect.R
@@ -14,3 +14,33 @@ make_redirect <- function(from, to){
 }
 
 mapply(make_redirect, from = names(redirects), to = redirects)
+
+make_redirect_apps <- function(name, url){
+  fs::dir_create(
+    fs::path(
+      "redirects", 
+      name
+    )
+  )
+  fls <- fs::path(
+    "_site", 
+    name, 
+    "index.html"
+  )
+  fs::file_create(
+    fls
+  )
+  write(file = fls, 
+        sprintf('<head><meta http-equiv="refresh" content="0; URL=%s" /></head>', url)
+  )
+}
+
+make_redirect_apps("tidytuesday201942", "https://connect.thinkr.fr/tidytuesday201942/")
+make_redirect_apps("hexmake", "https://connect.thinkr.fr/hexmake/")
+make_redirect_apps("minifying", "https://connect.thinkr.fr/minifying/")
+make_redirect_apps("golemhtmltemplate", "https://connect.thinkr.fr/golemhtmltemplate/")
+make_redirect_apps("shinipsumdemo", "https://connect.thinkr.fr/shinipsumdemo/")
+make_redirect_apps("databasedemo", "https://connect.thinkr.fr/databasedemo/")
+make_redirect_apps("graysacle", "https://connect.thinkr.fr/graysacle/")
+make_redirect_apps("bs4dashdemo", "https://connect.thinkr.fr/bs4dashdemo/")
+make_redirect_apps("shinyfuture", "https://connect.thinkr.fr/shinyfuture/")
