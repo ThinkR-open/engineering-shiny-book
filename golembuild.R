ea475963b657a52744497b5b8003dd4011729ac3
colin <colin@thinkr.fr>
2024-03-15 09:43:48 +0100

    feat: update script for modern usethis

--- golembuild.R
+++ golembuild.R
@@ -1,19 +1,12 @@
-testthat::with_mocked_bindings(
-  .package = "usethis",
-  allow_nested_project = function(){
-    return(TRUE)
-  },
-  code = {
-    library(magrittr)
-    unlink("golex", TRUE, TRUE)
-    golem::create_golem("golex", open = FALSE)
-    here::set_here("golex")
-    golem::add_module(name = "my_first_module", here::here("golex"), open = FALSE)
-    golem::add_js_handler("first_handler", here::here("golex"), open = FALSE)
-    golem::add_js_file("script", here::here("golex"), open = FALSE)
-    golem::add_js_handler("script", here::here("golex"), open = FALSE)
-    golem::add_css_file("custom", here::here("golex"), open = FALSE)
-  }
-)
+options(usethis.allow_nested_project = TRUE)
+library(magrittr)
+unlink("golex", TRUE, TRUE)
+golem::create_golem("golex", open = FALSE)
+golem::add_module(name = "my_first_module", here::here("golex"), open = FALSE)
+golem::add_js_handler("first_handler", here::here("golex"), open = FALSE)
+golem::add_js_file("script", here::here("golex"), open = FALSE)
+golem::add_js_handler("script", here::here("golex"), open = FALSE)
+golem::add_css_file("custom", here::here("golex"), open = FALSE)
+
 
 
