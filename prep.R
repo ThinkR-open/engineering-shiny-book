3725c4bfe7af0445fb0954ee1a80ef381758b70b
colin <colin@thinkr.fr>
2024-03-15 09:43:16 +0100

    feat: we'll not be using renv for now

--- prep.R
+++ prep.R
@@ -1,9 +1,15 @@
 # To do locally on Colin's computer, thanks
 options(repos = c(REPO_NAME = "https://packagemanager.rstudio.com/all/latest"))
-renv::restore()
-# remotes::install_local(Ncpus = 4, upgrade = "never", force = TRUE)
-if (!file.exists("packages.bib")){
-  knitr::write_bib(c(
-    desc::desc_get_deps()$package
-  ), 'packages.bib')
-}
+remotes::install_version("shinyloadtest", "1.1.0")
+remotes::install_local(
+  Ncpus = 4,
+  upgrade = "always",
+  force = TRUE
+)
+
+unlink("packages.bib", TRUE)
+knitr::write_bib(c(
+  desc::desc_get_deps()$package
+), 'packages.bib')
+
+minifyr::minifyr_npm_install(force = TRUE)
